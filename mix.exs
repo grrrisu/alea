3438fe11007c4a96a760199c24593b7b52115426
Alessandro Di Maria <adm@m42.ch>
2025-10-04 23:28:28 +0200

    monsum  css theme

--- mix.exs
+++ mix.exs
@@ -66,7 +66,8 @@ defmodule Alea.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:monsum, github: "grrrisu/monsum", app: false}
     ]
   end
 
