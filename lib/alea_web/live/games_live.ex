53662b0bd1665ab3f0d9ae7b0598e8ca8a1008f6
Alessandro Di Maria <adm@m42.ch>
2025-10-20 13:04:33 +0200

    game box css

--- lib/alea_web/live/games_live.ex
+++ lib/alea_web/live/games_live.ex
@@ -4,7 +4,7 @@ defmodule AleaWeb.GamesLive do
   alias Alea.Games
 
   def mount(_params, _session, socket) do
-    {:ok, socket |> assign(games: get_games())}
+    {:ok, socket |> assign(page_title: "Games", games: get_games())}
   end
 
   def render(assigns) do
@@ -13,14 +13,21 @@ defmodule AleaWeb.GamesLive do
       <.main_title>Games</.main_title>
       <.box_grid cols="4">
         <:box :for={game <- @games}>
-          <div class="p-4 border rounded flex flex-col" style="height: 500px">
-            <div class="basis-1/8">
-              <.sub_title class="text-lg font-bold">{game.name}</.sub_title>
+          <div
+            class="border border-info rounded flex flex-col bg-linear-to-br from-base-300 to-base-300/50"
+            style="height: 500px"
+          >
+            <div class="basis-1/4 p-4">
+              <.sub_title>{game.name}</.sub_title>
             </div>
-            <div class="basis-5/8 overflow-hidden">
-              <img src={game.image} alt={game.name} class="w-full" />
+            <div class="relative basis-1/2 w-full">
+              <img
+                src={game.image}
+                alt={game.name}
+                class="absolute inset-0 w-full h-full object-cover"
+              />
             </div>
-            <div class="basis-2/8">
+            <div class="basis-1/4 p-4">
               <.info_card value={game.yearpublished} icon="la-birthday-cake" />
             </div>
           </div>
